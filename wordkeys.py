33841c392621a0273217a6de61dec74c3dddd751
Mackieftw <Mackieftw@users.noreply.github.com>
2016-11-08 22:22:58 +0000

    Create wordkeys.py
    Use this for auth handling.

---
+++ wordkeys.py
@@ -0,0 +1,10 @@
+import tweepy
+#If you do not have or know how to get these keys, you must create an app on twitter
+#The information will then be listed under that app.
+
+
+#-----------------------------------auth details - App Learn New Words -----------------------------------
+Tkey = "YOUR KEY HERE"
+Tsecret = "YOUR SECRET HERE"
+access_token = "YOUR TOKEN HERE"
+access_token_secret = "YOUR TOKEN SECRET HERE"
